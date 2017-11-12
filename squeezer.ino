1a9b6d76c5dd2f86ccb883e309583c272895c7da
phi <philipwaritschlager@web.de>
2017-11-12 06:23:24 +0100

    + leds

--- squeezer.ino
+++ squeezer.ino
@@ -1,3 +1,5 @@
+#include <Adafruit_NeoPixel.h>
+
 /*
 20171111 phwa
 */
@@ -9,30 +11,76 @@
 #define PIN_MOTOR_I_GROUND 2
 #define PIN_BUTTON_EXT 6
 #define PIN_BUTTON_RET 7
+#define PIN_LED_1_DI 8
+#define PIN_LED_2_DI 9
+#define LED_1_COUNT 8
+#define LED_2_COUNT 12
 
+bool motor_moving = false;
 void motor_reset() {
-  Serial.println("reset");
+//  Serial.println("reset");
   digitalWrite(PIN_MOTOR_X_POWER,LOW);
   digitalWrite(PIN_MOTOR_X_GROUND,LOW);
   digitalWrite(PIN_MOTOR_I_POWER,LOW);
   digitalWrite(PIN_MOTOR_I_GROUND,LOW);
 }
 void motor_left() {
-  Serial.println("left");
+//  Serial.println("left");
   motor_reset();
   digitalWrite(PIN_MOTOR_X_POWER, HIGH);
   digitalWrite(PIN_MOTOR_I_GROUND, HIGH);
+  motor_moving = true;
 }
 void motor_right() {
-  Serial.println("right");
+//  Serial.println("right");
   motor_reset();
   digitalWrite(PIN_MOTOR_I_POWER, HIGH);
   digitalWrite(PIN_MOTOR_X_GROUND, HIGH);
+  motor_moving = true;
 }
 void motor_stop() {
-  Serial.println("stop");
+//  Serial.println("stop");
   motor_reset();
   // todo evtl power+power high
+  motor_moving = false;
+}
+
+Adafruit_NeoPixel leds_1 = Adafruit_NeoPixel(LED_1_COUNT, PIN_LED_1_DI, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel leds_2 = Adafruit_NeoPixel(LED_2_COUNT, PIN_LED_2_DI, NEO_GRB + NEO_KHZ800);
+
+/*void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+  for(j=0; j<LED_1_COUNT; j++) {
+    for(i=0; i< LED_1_COUNT; i++) {
+      leds_1.setPixelColor(i, Wheel(((i * 256 / LED_1_COUNT) + j) & 255));
+    }
+    leds_1.show();
+    delay(wait);
+  }
+}*/
+uint32_t Wheel(byte WheelPos, Adafruit_NeoPixel leds_ref) {
+  if(WheelPos < 85) {
+   return leds_ref.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if(WheelPos < 170) {
+   WheelPos -= 85;
+   return leds_ref.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return leds_ref.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
+void static_wheel(Adafruit_NeoPixel leds_ref, int LEDS_COUNT) {
+  int j;
+  for(j=0; j<LEDS_COUNT; j++) {
+    leds_ref.setPixelColor(j, Wheel(((j * 256 / LEDS_COUNT) + j) & 255, leds_ref));
+  }
+  leds_ref.show();
+}
+void color_wipe(uint32_t c, Adafruit_NeoPixel leds_ref, int LEDS_COUNT) {
+  for(uint16_t i=0; i<LEDS_COUNT; i++) {
+      leds_ref.setPixelColor(i, c);
+  }
+  leds_ref.show();
 }
 
 void setup() {
@@ -42,11 +90,15 @@ void setup() {
   pinMode(PIN_MOTOR_X_GROUND,OUTPUT);
   pinMode(PIN_MOTOR_I_POWER,OUTPUT);
   pinMode(PIN_MOTOR_I_GROUND,OUTPUT);
-
   pinMode(PIN_BUTTON_EXT,INPUT);
   pinMode(PIN_BUTTON_RET,INPUT);
+  pinMode(PIN_LED_1_DI,OUTPUT);
+  pinMode(PIN_LED_1_DI,OUTPUT);
 
-  motor_right();
+  leds_1.begin();
+  leds_1.show();
+  leds_2.begin();
+  leds_2.show();
 }
 
 // the loop routine runs over and over again forever:
@@ -64,8 +116,17 @@ void loop() {
   } else {
     // stop
     motor_stop();
-  }  
-  delay(100);
+  }
+  Serial.println(motor_moving);
+  if(motor_moving) {
+    static_wheel(leds_1, LED_1_COUNT);
+    static_wheel(leds_2, LED_2_COUNT);
+  } else {
+    color_wipe(0, leds_1, LED_1_COUNT);
+    color_wipe(0, leds_2, LED_2_COUNT);
+  }
+  
+  delay(1);
 }
 
 
